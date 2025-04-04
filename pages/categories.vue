996bc62d63e5d6d4d0c6850dbe96efd2f7192aee
d9k <d9k@ya.ru>
2025-04-04 07:58:52 +0500

    +/pages, +layouts/, +lint

---
+++ pages/categories.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h3>Categories</h3>
+  </div>
+</template>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,9 @@
 <template>
-  <!-- <div>
+  <div>
     <v-btn>
       Hello
     </v-btn>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
-  </div> -->
-  <NuxtLoadingIndicator />
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  </div>
 </template>
